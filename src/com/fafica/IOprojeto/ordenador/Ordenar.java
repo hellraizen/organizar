b43f6b9a0874f4b22b3c4a553c6898c29ff66374
danilo <danilo@DESKTOP-3QKLIFR.home>
2015-11-24 23:35:49 -0200

    testando

--- src/com/fafica/IOprojeto/ordenador/Ordenar.java
+++ src/com/fafica/IOprojeto/ordenador/Ordenar.java
@@ -7,10 +7,10 @@ public class Ordenar {
 		String aux;
 		
 		for(int i = 0 ; i < array.length ; i++){
-			for(int j = 0 ; j < array.length; j ++){
+			for(int j = 0 ; j < array.length; i ++){
 				if (array[i].compareTo(array[j])<0){
 					aux= array[j];
-					array[j]= array[i];
+					array[i]= array[i];
 					array[i]= aux;
 				}
 			}
